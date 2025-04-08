065695a84331c7fbd3182d1fe1d2d4bf6546a99b
ljw <ljw2017@sjtu.edu.cn>
2025-04-08 13:04:05 +0800

    use both jaspar core and unvalidation

--- preprocess/know_motif/hocomoco/run.sh
+++ preprocess/know_motif/hocomoco/run.sh
@@ -11,28 +11,79 @@ do
     accessions+=($accession)
 done
 
+# 把uniprot accession映射到基因名字
+gene_names=()
+while read accession gene_name
+do
+    gene_names+=($gene_name)
+done < <(
+    printf "%s\n" "${accessions[@]}" |
+    ../uniprot_gene_to_id.py \
+        "UniProtKB_AC-ID" \
+        "Gene_Name"
+)
+
 # 把下了数据的蛋白的motif提取出来
 mkdir -p c2h2_motifs
-while read uniprot_ac uniprot_id gene_symbol gene_synonyms
+# 先找对应的基因名
+for ((i=0; i<"${#accessions[@]}"; i++))
 do
-    gene_names=(${uniprot_id%_*} $gene_symbol $gene_synonyms)
-    for gene_name in "${gene_names[@]}"
+    while read uniprot_ac uniprot_id gene_symbol gene_synonyms
     do
-        id=$(grep -i "^MOTIF $gene_name.H13CORE.0." H13CORE_meme_format.meme | sed 's/^MOTIF //')
-        if [ -n "$id" ]
+        names=(${uniprot_id%_*} $gene_symbol $gene_synonyms)
+        name_match="false"
+        for name in "${names[@]}"
+        do
+            if [ "${gene_names[$i]^^}" = "${name^^}" ]
+            then
+                name_match="true"
+                break
+            fi
+        done
+        if [ "$name_match" = "true" ]
         then
-            meme-get-motif -id $id H13CORE_meme_format.meme |
-            sed -r "s/^MOTIF (.+)\.H13CORE\.0\.(.+)$/MOTIF $uniprot_ac \1/" \
-                > c2h2_motifs/hocomoco_$uniprot_ac.meme
+            for name in "${names[@]}"
+            do
+                id=$(grep -i "^MOTIF $name.H13CORE.0." H13CORE_meme_format.meme | sed 's/^MOTIF //')
+                if [ -n "$id" ]
+                then
+                    meme-get-motif -id $id H13CORE_meme_format.meme |
+                    sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
+                        > "c2h2_motifs/hocomoco_${accessions[$i]}.meme"
+                    break
+                fi
+            done
             break
         fi
-    done
-done < <(
-    grep \
-        -f <(printf "%s\n" "${accessions[@]}") \
-        tf_masterlist.tsv |
-    cut -d $'\t' -f 6,8,10,11
-)
+    done < <(
+        cut -d $'\t' -f 6,8,10,11 tf_masterlist.tsv
+    )
+done
+# 再找对应的uniprot accession来覆盖基因名的结果
+for ((i=0; i<"${#accessions[@]}"; i++))
+do
+    while read uniprot_ac uniprot_id gene_symbol gene_synonyms
+    do
+        names=(${uniprot_id%_*} $gene_symbol $gene_synonyms)
+        if [ "${accessions[$i]}" = "$uniprot_ac" ]
+        then
+            for name in "${names[@]}"
+            do
+                id=$(grep -i "^MOTIF $name.H13CORE.0." H13CORE_meme_format.meme | sed 's/^MOTIF //')
+                if [ -n "$id" ]
+                then
+                    meme-get-motif -id $id H13CORE_meme_format.meme |
+                    sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
+                        > "c2h2_motifs/hocomoco_${accessions[$i]}.meme"
+                        break
+                fi
+            done
+            break
+        fi
+    done < <(
+        cut -d $'\t' -f 6,8,10,11 tf_masterlist.tsv
+    )
+done
 
 # 把下了数据的蛋白的motif合并在一起
 meme2meme $(ls c2h2_motifs/hocomoco_*.meme) \
--- preprocess/know_motif/jaspar/run.sh
+++ preprocess/know_motif/jaspar/run.sh
@@ -23,76 +23,84 @@ done < <(
         "Gene_Name"
 )
 
-# 收集jaspar的motif id，uniprot accessions，基因名
-ids=()
-uniprot_idss=()
-names=()
-while IFS="|" read id uniprot_ids name
+# 先找UNVALIDATED数据库，再用CORE数据库覆盖UNVALIDATED的结果
+for collection in "UNVALIDATED" "CORE"
 do
-    ids+=("$id")
-    uniprot_idss+=("$uniprot_ids")
-    names+=("$name")
-done < <(
-    gawk '
-        $0 ~ /^AC / {
-            id = $0;
-            sub(/^AC /, "", id);
-        }
-        $0 ~ /^CC uniprot_ids:/ {
-            uniprot_ids = $0;
-            sub(/^CC uniprot_ids:/, "", uniprot_ids)
-            sub(/ /, "", uniprot_ids)
-        }
-        $0 ~ /^ID / {
-            name = $0;
-            sub(/^ID /, "", name)
-        }
-        $0 ~ /^\/\/$/ {
-            printf("%s|%s|%s\n", id, uniprot_ids, name)
-        }
-    ' \
-        < JASPAR2024_CORE_non-redundant_pfms_transfac.txt
-)
-
-# 把下了数据的蛋白的motif提取出来
-mkdir -p c2h2_motifs
-# 先找对应的基因名
-for ((i=0; i<"${#accessions[@]}"; i++))
-do
-    for ((j=0; j<"${#ids[@]}"; ++j))
+    transfac="JASPAR2024_${collection}_non-redundant_pfms_transfac.txt"
+    meme="JASPAR2024_${collection}_non-redundant_pfms_meme.txt"
+    # 收集jaspar的motif id，uniprot accessions，基因名
+    ids=()
+    uniprot_idss=()
+    names=()
+    while IFS="|" read id uniprot_ids name
     do
-        name_match="false"
-        while IFS="::" read single_name
+        ids+=("$id")
+        uniprot_idss+=("$uniprot_ids")
+        names+=("$name")
+    done < <(
+        gawk '
+            $0 ~ /^AC / {
+                id = $0;
+                sub(/^AC /, "", id);
+            }
+            $0 ~ /^CC uniprot_ids:/ {
+                uniprot_ids = $0;
+                sub(/^CC uniprot_ids:/, "", uniprot_ids)
+                sub(/ /, "", uniprot_ids)
+            }
+            $0 ~ /^ID / {
+                name = $0;
+                sub(/^ID /, "", name)
+            }
+            $0 ~ /^\/\/$/ {
+                printf("%s|%s|%s\n", id, uniprot_ids, name)
+            }
+        ' \
+            < $transfac
+    )
+
+    # 把下了数据的蛋白的motif提取出来
+    mkdir -p c2h2_motifs
+    # 先找对应的基因名
+    for ((i=0; i<"${#accessions[@]}"; i++))
+    do
+        for ((j=0; j<"${#ids[@]}"; ++j))
         do
-            if [ "${gene_names[$i]^^}" = "${single_name^^}" ]
+            name_match="false"
+            while IFS="::" read single_name
+            do
+                if [ "${gene_names[$i]^^}" = "${single_name^^}" ]
+                then
+                    name_match="true"
+                    break
+                fi
+            done <<< "${names[$j]}"
+            if [ "$name_match" = "true" ]
             then
-                name_match="true"
+                meme-get-motif -id "${ids[$j]}" $meme |
+                sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
+                    > "c2h2_motifs/jaspar_${accessions[$i]}.meme"
                 break
             fi
-        done <<< "${names[$j]}"
-        if [ "$name_match" = "true" ]
-        then
-            meme-get-motif -id "${ids[$j]}" JASPAR2024_CORE_non-redundant_pfms_meme.txt |
-            sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
-                > "c2h2_motifs/jaspar_${accessions[$i]}.meme"
-            break
-        fi
+        done
     done
-done
-# 再找对应的uniprot accession来覆盖基因名的结果
-for ((i=0; i<"${#accessions[@]}"; i++))
-do
-    for ((j=0; j<"${#ids[@]}"; ++j))
+    # 再找对应的uniprot accession来覆盖基因名的结果
+    for ((i=0; i<"${#accessions[@]}"; i++))
     do
-        if [[ "${uniprot_idss[$j]}" =~ "${accessions[$i]}" ]]
-        then
-            meme-get-motif -id "${ids[$j]}" JASPAR2024_CORE_non-redundant_pfms_meme.txt |
-            sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
-                > "c2h2_motifs/jaspar_${accessions[$i]}.meme"
-            break
-        fi
+        for ((j=0; j<"${#ids[@]}"; ++j))
+        do
+            if [[ "${uniprot_idss[$j]}" =~ "${accessions[$i]}" ]]
+            then
+                meme-get-motif -id "${ids[$j]}" $meme |
+                sed -r "s/^MOTIF .+$/MOTIF ${accessions[$i]} ${gene_names[$i]}/" \
+                    > "c2h2_motifs/jaspar_${accessions[$i]}.meme"
+                break
+            fi
+        done
     done
 done
+
+
 
 # 把下了数据的蛋白的motif合并在一起
 meme2meme $(ls c2h2_motifs/jaspar_*.meme) \
