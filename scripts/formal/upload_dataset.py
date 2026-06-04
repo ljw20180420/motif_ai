91b261b6694534301b29f80d9cc827d0da00cf8d
ljw <ljw2017@sjtu.edu.cn>
2026-06-04 11:19:02 +0800

    Add upload dataset.

---
+++ scripts/formal/upload_dataset.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import pathlib
+import sys
+
+import jsonargparse
+from common_ai.dataset import MyDatasetAbstract
+
+# change to the project folder
+os.chdir(pathlib.Path(__file__).resolve().parent.parent.parent)
+sys.path.append(os.getcwd())
+
+from AI.dataset import MyDataset
+
+parser = jsonargparse.ArgumentParser(description="Parse dataset arguments.")
+parser.add_argument("--config", action="config")
+parser.add_subclass_arguments(baseclass=MyDatasetAbstract, nested_key="dataset")
+
+args = parser.parse_args([
+    "--dataset",
+    "AI/dataset/dataset.yaml",
+    "--dataset.data_dir",
+    "AI/dataset/formal_data",
+])
+
+ds = MyDataset(**args.dataset.init_args.as_dict())()
+
+while True:
+    try:
+        ds.push_to_hub("ljw20180420/COP")
+        break
+    except RuntimeError:
+        pass
